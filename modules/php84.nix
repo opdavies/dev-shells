c740cf6d5acb8fee75fc40bd7e8ce6f68fedabfa
Oliver Davies <oliver@oliverdavies.uk>
2025-07-30 01:31:27 +0100

    Add PHP 8.4

---
+++ modules/php84.nix
@@ -0,0 +1,18 @@
+{ commonPhpPackages, ... }:
+
+{
+  perSystem =
+    { pkgs, ... }:
+    {
+      devShells.php84 = pkgs.mkShell {
+        packages = with pkgs; [
+          php84
+          php84Packages.composer
+        ] ++ commonPhpPackages pkgs;
+
+        shellHook = ''
+          php --version
+        '';
+      };
+    };
+}
