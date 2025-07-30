6c6bbd9338d953c77340137ecced5a5f4f8f7165
Oliver Davies <oliver@oliverdavies.uk>
2025-07-30 01:16:41 +0100

    Refactor

--- flake.nix
+++ flake.nix
@@ -3,28 +3,23 @@
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
 
+    import-tree.url = "github:vic/import-tree";
+
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nixpkgs-php74.url = "github:nixos/nixpkgs/81b77fd3847a";
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
 
       _module.args = {
         commonPhpPackages = pkgs: with pkgs; [ phpactor ];
       };
 
-      imports = [
-        ./modules/nodejs.nix
-        ./modules/php74.nix
-        ./modules/php81.nix
-        ./modules/php82.nix
-        ./modules/php83.nix
-        ./modules/tailwindcss.nix
-      ];
+      imports = [ (inputs.import-tree ./modules) ];
 
       perSystem =
         { pkgs, ... }:
@@ -32,8 +27,6 @@
           devShells.default = pkgs.mkShell {
             packages = with pkgs; [ nixd ];
           };
-
-          formatter = pkgs.nixfmt-rfc-style;
         };
     };
 }
